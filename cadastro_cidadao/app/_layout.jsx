41e28ec5c1f47eedd783ffaf0eb0610cc58d3548
Mateus Irineu <irineu.mateus11@gmail.com>
2025-10-02 20:59:05 -0300

    .

--- cadastro_cidadao/app/_layout.jsx
+++ cadastro_cidadao/app/_layout.jsx
@@ -1,24 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS } from "../../styles/colors";
+import styles from "../../styles/homeStyles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Background } from "@react-navigation/elements";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+export default function HomeScreen() {
+  const nav = useNavigation();
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const blocks = [
+    { title: "Cadastro", screen: "Cadastro", icon: "person-add" },
+    { title: "Configurações", screen: "Configurações", icon: "settings" },
+    { title: "Histórico", screen: "Histórico", icon: "history" },
+    { title: "Sobre", screen: "Sobre", icon: "info" },
+  ];
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={[styles.container]}>
+      <Text style={styles.title}>App Cadastro de Cidadão</Text>
+      <View style={styles.grid}>
+        {blocks.map((b, i) => (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            key={i}
+            styles={[
+              styles.block,
+              { background: Object.values(COLORS)[i % 4] },
+            ]}
+            onPress={() => nav.navigate(b.screen)}
+          >
+            <MaterialIcons naem={b.icon} size={42} color="#064E2E" />
+            <Text style={styles.blockText}>{b.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
   );
 }
